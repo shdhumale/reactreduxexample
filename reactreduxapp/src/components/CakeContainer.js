c5573eee3ffb51b3194341c150bc813aa2fc86b6
Siddharatha.H.Dhumale <shdhumale@gmail.com>
2020-04-17 14:43:16 +0530

    React with Redux

---
+++ reactreduxapp/src/components/CakeContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux'
+
+function CakeContainer(props) {
+    return (
+        <div>
+            <h1>Number of Cake -- {props.numOfCakes}</h1>
+            <button onClick={props.buyCake}>Buy Cake</button>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => { dispatch(buyCake()) }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
